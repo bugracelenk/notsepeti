caff2a97b68f59ef2ab8fac9f1fc2046a3d6b6d4
Altuğ Buğra Çelenk <bugracelenk01@gmail.com>
2019-07-11 18:50:40 +0300

    added trending now and top category sections, still just for the mobile.

--- src/views/Main.js
+++ src/views/Main.js
@@ -16,14 +16,24 @@ export default class Main extends Component {
               </h1>
             </div>
             <div className="jumbotron-search">
-              <div className="jumbotron-searchWrapper"><input id="jumbosearch" name="jumbosearch" placeholder="What do you want to study ?"/></div>
-              <span className="button-search"><i class="fas fa-search"></i></span>
+              <div className="jumbotron-searchWrapper">
+                <input
+                  id="jumbosearch"
+                  name="jumbosearch"
+                  placeholder="What do you want to study ?"
+                />
+              </div>
+              <span className="button-search">
+                <i class="fas fa-search" />
+              </span>
             </div>
           </div>
         </div>
 
         <div className="section">
-          <h1>Recommended <span>For You</span></h1>
+          <h1>
+            Recommended <span>For You</span>
+          </h1>
           <div className="recomendetions">
             <div className="recommended">
               <div className="recommended-top">
@@ -56,6 +66,88 @@ export default class Main extends Component {
               <div className="recommended-bottom">
                 <h3>Coding</h3>
               </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <h1>
+            Top <span>Category</span>
+          </h1>
+          <div className="categories">
+            <div className="categories-category">
+              <i class="fas fa-arrows-alt" />
+              <h3>Development</h3>
+            </div>
+            <div className="categories-category">
+              <i class="fas fa-arrows-alt" />
+              <h3>Development</h3>
+            </div>
+            <div className="categories-category">
+              <i class="fas fa-arrows-alt" />
+              <h3>Development</h3>
+            </div>
+            <div className="categories-category">
+              <i class="fas fa-arrows-alt" />
+              <h3>Development</h3>
+            </div>
+            <div className="categories-category">
+              <i class="fas fa-arrows-alt" />
+              <h3>Development</h3>
+            </div>
+            <div className="categories-category">
+              <i class="fas fa-arrows-alt" />
+              <h3>Development</h3>
+            </div>
+          </div>
+        </div>
+
+        <div className="section">
+          <h1>
+            Trending <span>Now</span>
+          </h1>
+          <div className="trends">
+            <div
+              className="trends-trend"
+              style={{
+                backgroundImage:
+                  "url(" + "https://i.hizliresim.com/zG5Y6j.png" + ")"
+              }}
+            >
+              <img
+                alt="trends-trend-icon"
+                className="trends-trend-icon"
+                src="https://i.hizliresim.com/gP8y75.png"
+              />
+              <h3>Law</h3>
+            </div>
+            <div
+              className="trends-trend"
+              style={{
+                backgroundImage:
+                  "url(" + "https://i.hizliresim.com/zG5Y6j.png" + ")"
+              }}
+            >
+              <img
+                alt="trends-trend-icon"
+                className="trends-trend-icon"
+                src="https://i.hizliresim.com/gP8y75.png"
+              />
+              <h3>Law</h3>
+            </div>
+            <div
+              className="trends-trend"
+              style={{
+                backgroundImage:
+                  "url(" + "https://i.hizliresim.com/zG5Y6j.png" + ")"
+              }}
+            >
+              <img
+                alt="trends-trend-icon"
+                className="trends-trend-icon"
+                src="https://i.hizliresim.com/gP8y75.png"
+              />
+              <h3>Law</h3>
             </div>
           </div>
         </div>
